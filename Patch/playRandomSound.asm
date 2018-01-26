53c26c005231cebe5fa47bb2c0d273252146955d
idealexe <ideal.exe@live.jp>
2018-01-27 03:04:58 +0900

    v2: Songs are randomly played in virus and boss battle

--- Patch/playRandomSound.asm
+++ Patch/playRandomSound.asm
@@ -7,15 +7,22 @@
 playRandomSound:
   push r14
   cmp r0, 0x15  // is virus battle song?
-  bne originalProcessing
+  bne isBossBattleSong
 
-  selectSound:
-    ldr r0, =playList
+  isVirusSong:
+    ldr r0, =virusPlayList
+    b pickRandomSong
+
+  isBossBattleSong:
+    cmp r0, 0x16  // is boss battle song?
+    bne originalProcessing
+    ldr r0, = bossPlayList
+
+  pickRandomSong:
     ldr r3, =0x2001120  // fluctuating value
     ldrb r3, [r3, 0h]
     lsr r3, r3, 5 // get 3bit value to select from playList
     ldrb r0, [r0, r3]
-    b originalProcessing
 
   originalProcessing:
     lsl r0, r0, 0x10
@@ -24,8 +31,11 @@ playRandomSound:
   pop r15
 
 .pool
-playList:  // play list
-  .byte 21, 34, 38, 41, 45, 48, 49, 52  // virus battle
+virusPlayList:  // virus play list
+  .byte 21, 34, 38, 41, 45, 48, 49, 52  // virus battle songs
+
+bossPlayList:
+  .byte 22, 39, 42, 44, 46, 50, 53, 22  // boss battle songs
 
 // Hook
 .org 0x0815B33A  // playSound
