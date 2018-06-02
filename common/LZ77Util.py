ba96504ce8a05b8c86ddd1919dbca5ba33aad1c4
idealexe <omega_8810@yahoo.co.jp>
2018-06-03 05:38:41 +0900

    update

--- common/LZ77Util.py
+++ common/LZ77Util.py
@@ -6,8 +6,6 @@
 
     LZ77圧縮されたデータを検索したり解凍するモジュールです．
 """
-
-
 import os
 import re
 import struct
@@ -15,7 +13,7 @@ import sys
 import time
 
 
-def detectLZ77(romData, minSize=0x100, maxSize=0x10000, searchStep=0x4, checkRef=True):
+def detectLZ77(romData, minSize=0x100, maxSize=0x100000, searchStep=0x4, checkRef=False):
     """ LZ77(0x10)圧縮されてそうなデータの検索
 
         LZ77圧縮データの先頭はだいたい 10 XX YY ZZ 00 00 XX YY ZZ になる
@@ -24,12 +22,13 @@ def detectLZ77(romData, minSize=0x100, maxSize=0x10000, searchStep=0x4, checkRef
             00 = 展開後データの先頭、非圧縮を示す
             00 = １つめのフラグバイト（先頭なので必然的に全ビット0となる）
     """
-
     matchList = []
     candidateIter = re.finditer(b"\x10(?P<size>...)\x00\x00(?P=size)", romData)
+
     for match in candidateIter:
         matchAddr = match.start()
-        uncompSize = struct.unpack('l', romData[matchAddr+1:matchAddr+4] + b"\x00")[0] # 次3バイトが展開後のサイズ（4バイトに合わせないとunpack出来ないので"\x00"をつけている）
+        uncompSize = struct.unpack('l', romData[matchAddr+1:matchAddr+4] + b"\x00")[0]  # 次3バイトが展開後のサイズ（4バイトに合わせないとunpack出来ないので"\x00"をつけている）
+
         # キリが良い位置にあってサイズが妥当なものを抽出
         if matchAddr % searchStep == 0 and minSize <= uncompSize <= maxSize:
             if checkRef is True:
@@ -39,11 +38,10 @@ def detectLZ77(romData, minSize=0x100, maxSize=0x10000, searchStep=0x4, checkRef
                 if re.search(pattern, romData) is None:
                     # ポインタがなかったら無視
                     continue
-            matchList.append({"startAddr":matchAddr, "uncompSize":uncompSize})
+            matchList.append({"startAddr": matchAddr, "uncompSize": uncompSize})
 
     for i, item in enumerate(matchList):
-        print(str(i) + ":\t" + hex(item["startAddr"]) + \
-                "\t" + hex(item["uncompSize"]) + " bytes")
+        print(str(i) + ":\t" + hex(item["startAddr"]) + "\t" + hex(item["uncompSize"]) + " bytes")
     return matchList
 
 
@@ -135,10 +133,10 @@ def saveFile(data, outName):
 
 
 def main():
-    filepath = sys.argv[1]
-    with open(filepath, "rb") as romFile:
-        romData = romFile.read()
-    detectLZ77(romData)
+    file_path = sys.argv[1]
+    with open(file_path, "rb") as bin_file:
+        bin_data = bin_file.read()
+    detectLZ77(bin_data)
 
 
 if __name__ == '__main__':
